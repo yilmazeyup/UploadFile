39f2fe45de13de45f9ed3c3df1601ee55e4638de
MUHAMMED EYÜP YILMAZ <muhammedeyup.yilmaz@student.iu.edu.tr>
2021-06-25 09:46:09 +0300

    Add project files.

---
+++ Connection.cs
@@ -0,0 +1,68 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadFile
+{
+    public class Connection
+    {
+        public static void Npgsql()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection())
+
+            {
+                string query = @"insert into public.Students(Name,Fees)values('Gautam',200.0)";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+                if (n == 1)
+                {
+                    Console.WriteLine("Record Inserted");
+                }
+
+            }
+        }
+        public class dbSqliteConnection
+        {
+            public static string dbAddress = @"Data Source=" + "C:/Dr/....;New=False;Compress=True;Read Only=False";
+            public static string ConnectionStatus;
+            public static void Sqlite()
+            {
+                using (SqliteConnection conn = new SQLiteConnection(dbAddress))
+                {
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        try
+                        {
+                            conn.Open();
+                            ConnectionStatus = "SQLite Connection successful";
+                        }
+                        catch (Exception)
+                        {
+                            ConnectionStatus = "SQLite Connection failed";
+                        }
+                    }
+                    else
+                    {
+                        ConnectionStatus = "SQLite Connection successful";
+                    }
+                }
+
+            }
+        }
+
+        public static void Firebase()
+        {
+            IFirebaseClient client;
+            IFirebaseConfig ifc = new FirebaseConfig()
+            {
+                AuthSecret = "",
+                BasePath = ""
+            };
+        private object serial;
+    }
+    }
+}
---
+++ UploadFileController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace UploadFile.wwwroot
+{
+    public class UploadFileController : Controller
+    {
+        
+            [HttpPost]
+            [Route("UploadFile")]
+            public IActionResult UploadFile(int? contractId, string path)
+            {
+                if (contractId == null)
+                    return Content("Sözleşme Numarası bulunamadı");
+
+
+                var file = Request.Form.Files[0];
+
+                if (file == null || file.Length == 0)
+                    return Content("Dosya bulunamadı");
+
+                var fileName = ContentDispositionHeaderValue
+                           .Parse(file.ContentDisposition)
+                           .FileName
+                           .Trim('"');
+                using (FileStream fs = System.IO.File.Create(path + fileName))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+
+                string filePath = Path.ChangeExtension(fileName.ToString(), null);
+                string pathRev = path.Replace(":", "");
+                DateTime createdOn = DateTime.Now;
+
+                Connection.Npgsql();
+
+                return View();
+            }
+
+            [HttpPost]
+            [Route("DeleteFile")]
+            public IActionResult DeleteFile(int? contractId, string path)
+            {
+                if (contractId == null)
+                    return Content("Sözleşme Numarası bulunamadı");
+
+
+                var file = Request.Form.Files[0];
+
+                if (file == null || file.Length == 0)
+                    return Content("Dosya bulunamadı");
+
+                var fileName = ContentDispositionHeaderValue
+                           .Parse(file.ContentDisposition)
+                           .FileName
+                           .Trim('"');
+
+                System.IO.File.Delete(path + fileName);
+
+                string filePath = Path.ChangeExtension(fileName.ToString(), null);
+                string pathRev = path.Replace(":", "");
+                DateTime createdOn = DateTime.Now;
+
+                Connection.Firebase();
+
+                return View();
+            }
+        
+    }
+}
